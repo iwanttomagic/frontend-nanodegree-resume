b3e819663bbecc6a54c20a743d32c399cab80216
Hannah <wndergrrrl@gmail.com>
2015-04-07 19:15:58 -0700

    add name and role to header

---
+++ js/resumeBuilder.js
@@ -0,0 +1,7 @@
+var name = "Hannah Thompson";
+var role = "Front End Web Designer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
